2b6a80c9745f96db6b5322e0deebad1e67965eb2
JackHammond47 <jackson.hammond47@gmail.com>
2024-05-29 12:00:36 -0600

    card class

---
+++ Card.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+
+class Card:
+    def __init__(self):
+        super().__init__()
+        
+        #define cards and replace with images
+        self.kick = pygame.image.load
