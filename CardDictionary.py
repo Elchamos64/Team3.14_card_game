c7af31746cc414acfd72e6f07cdf82d66efb17b7
CaydenLords <lor20002@byui.edu>
2024-06-07 12:32:45 -0600

    More Pictures

--- CardDictionary.py
+++ CardDictionary.py
@@ -1,7 +1,7 @@
 from Card import Card
 
 cards = {
-        "Sword Slash": Card("Kick", "Images/Card/Kick.png", "Deal 2-3 Damage",  [0, ]),
-        "Healing Potion": Card("Healing Potion", "Images/Card/Hide.png", "Heal 0-5 Health", []),
+        "Sword Slash": Card("Sword Slash", "Images/Card/SwordSlash.png", "Deal 2-3 Damage",  [0, ]),
+        "Healing Potion": Card("Healing Potion", "Images/Card/HealingPotion.png", "Heal 0-5 Health", []),
         "Shield Block": Card("Shield Block", "Images/Card/Block.png", "Block 5-9 Damage", [])
     }
