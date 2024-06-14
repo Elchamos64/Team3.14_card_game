0f2f9b6a21e2a44c5bc2d7a0d78608f1130d535c
jooshhart <jooshhart@gmail.com>
2024-06-14 11:35:43 -0600

    y

--- Card.py
+++ Card.py
@@ -1,4 +1,5 @@
 import pygame
+import Random
 
 class Card:
     cards = {
@@ -7,14 +8,36 @@ class Card:
         "Shield Block": (pygame.image.load("Images/Cards/shield_block.jpg"), [0, 0, 5, 1])
     }
 
-    @classmethod
-    def get_image(cls, name):
-        if name in cls.cards:
-            return cls.cards[name][0]
-        return None
+    def attack(self, lowNum, highNum, APcost):
+        if self.protagonist.current_action_points > 0:
+            damage = Random.randint(lowNum, highNum)
+            self.enemy.current_health -= damage
+            if self.enemy.current_health < 0:
+                self.enemy.current_health = 0
+            print(f"Player attacks for {damage} damage!")
+            # Reduce action points after performing an action
+            self.protagonist.reduce_action_points(APcost)
 
-    @classmethod
-    def get_values(cls, name):
-        if name in cls.cards:
-            return cls.cards[name][1]
-        return None
+    def heal(self,lowNum, highNum, APcost):
+        if self.protagonist.current_action_points > 0:
+            heal_amount = Random.randint(lowNum, highNum)
+            self.protagonist.current_health = min(self.protagonist.max_health, self.protagonist.current_health + heal_amount)
+            print(f"Player heals for {heal_amount} health!")
+            # Reduce action points after performing an action
+            self.protagonist.reduce_action_points(APcost)
+
+    def block(self, lowNum, highNum, APcost):
+        if self.protagonist.current_action_points > 0:
+            block_points = Random.randint(lowNum, highNum)
+            self.protagonist.block_points += block_points
+            print(f"Player blocks, gaining {block_points} block points!")
+            # Reduce action points after performing an action
+            self.protagonist.reduce_action_points(APcost)
+
+    def display(self, screen, x, y):
+        screen.blit(self.cardfront, (x, y, 100, 130))   
+        box_points_text = self.font.render(str(self.text), True, (0, 0, 0))  # Black color
+        screen.blit(self.picture, (x+5, y+20, 50, 50))
+        screen.blit(box_points_text, (x, y+75))
+        box_points_name = self.nameFont.render(str(self.name), True, (0, 0, 0))  # Black color
+        screen.blit(box_points_name, (x, y+5))
