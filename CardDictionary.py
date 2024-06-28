50438f1a30de0609394f75902c35e1a8340faae0
CaydenLords <lor20002@byui.edu>
2024-06-28 12:00:47 -0600

    Balance Patch

--- CardDictionary.py
+++ CardDictionary.py
@@ -1,10 +1,10 @@
 from Card import Card
 
 cards = {
-        "Sword Slash": Card("Sword Slash", "Images/Card/SwordSlash.png", "Deal 2-3 Damage",  ["attack", 2,3,1]),
+        "Sword Slash": Card("Sword Slash", "Images/Card/SwordSlash.png", "Deal 7-10 Damage",  ["attack", 7,10,1]),
         "Heal Potion": Card("Heal Potion", "Images/Card/HealingPotion.png", "Heal 0-5 Health", ["heal", 0,5,1]),
-        "Shield Block": Card("Shield Block", "Images/Card/Block.png", "Block 5-9 Damage", ["block", 5,9,1]),
-        "Kick": Card("Kick", "Images/Card/Kick.png", "Deal 0-5 Damage", ["attack", 0,5,1]),
+        "Shield Block": Card("Shield Block", "Images/Card/Block.png", "Block 8-9 Damage", ["block", 8,9,1]),
+        "Kick": Card("Kick", "Images/Card/Kick.png", "Deal 0-16 Damage", ["attack", 2,14,1]),
         "Hide": Card("Hide", "Images/Card/Hide.png", "Block 10 Damage", ["block", 10, 10, 2]),
         "Investigate": Card("Investigate", "Images/Card/Investigate.png", "Draw 2-3 Cards", ["draw", 2, 3, 1]),
         "Eureka!": Card("Eureka!", "Images/Card/Eureka.png", "Draw 0-2 Cards", ["draw", 0, 2, 0]),
